1e903353974fe7522ad7fca8d6e3a631a41c2e0c
Lebit009 <lebitmagar@gmail.com>
2020-11-12 11:52:23 +0545

    homework

--- src/kotlinbasic/sumevenarray.kt
+++ src/kotlinbasic/sumevenarray.kt
@@ -2,16 +2,17 @@ package kotlinbasic
 
 fun main() {
     var arr = arrayOf(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    displaysumodd(arr)
+    displaysumEven(arr)
 }
 
-fun displaysumodd(arr: Array<Int>) {
-    println("Odd number are: ")
+fun displaysumEven(arr: Array<Int>) {
+    println("Even number are: ")
     var sum = 0
     for (a in arr.indices) {
-        if (arr[a] % 2 != 0) {
+        if (arr[a] % 2 == 0) {
             sum += arr[a]
         }
     }
     println("$sum")
 }
+
