37583f71cbaedf9e28d0823958a353401571def3
Lebit009 <lebitmagar@gmail.com>
2020-11-12 10:32:21 +0545

    Loop

---
+++ src/kotlinbasic/loop.kt
@@ -0,0 +1,14 @@
+package kotlinbasic
+
+fun main() {
+    for (i in 1..10) {
+//        or 1 until 11
+        println(i)
+    }
+    for (i in 1..10 step 2) {
+        println(i)
+    }
+    for (i in 10 downTo 1) {
+        println(i)
+    }
+}
